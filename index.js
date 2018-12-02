74d521d6e794c94e03c63fa5ca66cc14d5a4392a
iTechroof <kiselvav@microsoft.com>
2018-12-02 20:11:56 +0530

    added store

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
 import {name as appName} from './app.json';
+import configureStore from './src/store/configureStore';
+import React from 'react';
+
+const store = configureStore();
+const RNRedux = (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
 
 AppRegistry.registerComponent(appName, () => App);
